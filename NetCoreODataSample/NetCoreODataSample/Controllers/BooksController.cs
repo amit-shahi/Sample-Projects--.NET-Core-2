ba3ca2f18229c4e27be52cc270992e4c79e2d568
Amit Shahi <ami.oneworld@gmail.com>
2019-07-19 12:55:08 +0530

    Added .NET Core 2 projects with OData Support for enhancing REST API to the next level

---
+++ NetCoreODataSample/NetCoreODataSample/Controllers/BooksController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreODataSample.Data;
+
+namespace NetCoreODataSample.Controllers
+{
+    public class BooksController : ODataController
+    {
+        private readonly BookStoreContext _bookStoreContext;
+
+
+        public BooksController(BookStoreContext context)
+        {
+            _bookStoreContext = context;
+            if (context.Books.Count() == 0)
+            {
+                foreach (var b in BooksDataSource.GetBooks())
+                {
+                    context.Books.Add(b);
+                    context.Presses.Add(b.Press);
+                }
+                context.SaveChanges();
+            }
+        }
+
+
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            return Ok(_bookStoreContext.Books);
+        }
+
+        [EnableQuery]
+        public IActionResult Get(int key)
+        {
+            return Ok(_bookStoreContext.Books.FirstOrDefault(c => c.Id == key));
+        }
+
+
+        [EnableQuery]
+        public IActionResult Post([FromBody]Book book)
+        {
+            _bookStoreContext.Books.Add(book);
+            _bookStoreContext.SaveChanges();
+            return Created(book);
+        }
+
+
+
+    }
+}
---
+++ NetCoreODataSample/NetCoreODataSample/Data/BookStoreContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreODataSample.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Press> Presses { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().OwnsOne(c => c.Location);
+           
+        }
+
+    }
+}
